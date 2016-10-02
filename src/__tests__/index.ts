6114067221f7d253a364b626a487ea7f484db7f7
Robin Ricard <ricard.robin@gmail.com>
2016-10-02 10:53:23 -0700

    Setup Developer Experience
    - TypeScript
    - Mocha
    - VSCode debugging

---
+++ src/__tests__/index.ts
@@ -0,0 +1,8 @@
+import {TEST} from '../index';
+import {assert} from 'chai';
+
+describe('GraphQL Document Collector Main Module', () => {
+  it('exports something', () => {
+    assert(TEST);
+  });
+});
