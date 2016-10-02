dda88d054154df3bebcf205c5cc6daad440f7b3e
Robin Ricard <ricard.robin@gmail.com>
2016-10-02 16:49:29 -0700

    Load, Transform & Flatten main func

--- src/__tests__/index.ts
+++ src/__tests__/index.ts
@@ -1,8 +1,18 @@
-import {TEST} from '../index';
+import loadTransformAndFlatten from '../index';
 import {assert} from 'chai';
 
-describe('GraphQL Document Collector Main Module', () => {
-  it('exports something', () => {
-    assert(TEST);
+import path = require('path');
+
+const documentsJson = require('../../example/documents.json');
+
+describe('Load, Transform and Flatten', () => {
+  it('should load documents in a structured object', () => {
+    return loadTransformAndFlatten(
+      path.join(__dirname, '..', '..', 'example'),
+      '**/*.graphql'
+    )
+    .then(docMap => {
+      assert.deepEqual(docMap, documentsJson);
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,28 @@
-export const TEST = true;
+import {
+  DirectoryTransform,
+  applyTransforms,
+  resolveFragments,
+} from './transforms';
+import {
+  DocumentFlatMap,
+  flattenDirectoryStructure,
+} from './flatten';
+import {
+  loadGlob,
+} from './loader';
+
+export * from './transforms';
+export * from './ast';
+export * from './flatten';
+export * from './loader';
+
+export default function loadTransformAndFlatten(
+  basePath: string,
+  globPath: string,
+  transforms: DirectoryTransform[] = [resolveFragments]
+): Promise<DocumentFlatMap> {
+  return loadGlob(basePath, globPath)
+  .then(root =>
+    flattenDirectoryStructure(applyTransforms(root, ...transforms))
+  );
+}
